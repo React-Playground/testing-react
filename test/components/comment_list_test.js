e3265731e243328d03fca86581d4b39ec363e182
darwin <oleg.umarov@gmail.com>
2016-03-27 22:58:48 -0500

    add comment list tests

--- test/components/comment_list_test.js
+++ test/components/comment_list_test.js
@@ -3,5 +3,21 @@ import CommentList from '../../src/components/comment_list.js';
 
 
 describe('CommentList', () => {
-  
+  let component;
+
+  beforeEach(() => {
+    const props = { comments: ['New Comment', 'Other New Comments']};
+    component = renderComponent(CommentList, null, props);
+  });
+
+  it('show an LI element for each comment', () => {
+    expect(component.find('li').length).to.equal(2);
+  });
+
+  it('shows each comment that is provided', () => {
+    expect(component).to.contain('New Comment');
+    expect(component).to.contain('Other New Comments');
+  });
 })
+
+
