29ce3a8375c06062d28857d1a57cde713ff2ffdf
darwin <oleg.umarov@gmail.com>
2016-03-30 22:02:34 -0500

    make comment box a container

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+export const SAVE_COMMENT = 'SAVE_COMMENT';
+
+
+export function saveComment(comment) {
+  return {
+    type: SAVE_COMMENT,
+    payload: comment
+  };
+}
--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-
-export default class CommentBox extends Component {
+class CommentBox extends Component {
   constructor(props) {
     super(props);
 
@@ -30,3 +31,9 @@ export default class CommentBox extends Component {
     );
   }
 }
+
+function mapStateToProps(state) {
+  return { comments: state.comments }
+}
+
+export default connect(null, actions)(CommentBox);
